d3287692705b365964c60ea04bc9483ddd691506
zhuangtianyu <zhuangtianyu1995@163.com>
2020-07-22 22:29:57 +0800

    feat: resume 页面移除 antd

--- src/views/resume/index.js
+++ src/views/resume/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Avatar, Tag } from 'antd'
 import './index.scss'
 
 function Resume () {
   return (
     <div className="resume__view">
       <div className="resume__view__base-info">
-        <Avatar
+        <img
           className="resume__view__base-info__avatar"
           src="http://zhuangtianyu.com/image/1580058226968.jpeg"
         />
@@ -18,26 +17,15 @@ function Resume () {
 
       <div className="resume__view__skill">
         <p>热爱原生 javascript.</p>
-        <p>熟悉 vuejs，react-hooks，了解 nodejs.</p>
-        <div className="resume__view__skill__tags">
-          <Tag color="magenta">element-ui</Tag>
-          <Tag color="red">iview</Tag>
-          <Tag color="volcano">antd</Tag>
-          <Tag color="orange">echarts</Tag>
-          <Tag color="gold">git</Tag>
-          <Tag color="lime">koa2</Tag>
-          <Tag color="green">mysql</Tag>
-          <Tag color="cyan">webpack</Tag>
-          <Tag color="blue">yarn</Tag>
-          <Tag color="geekblue">teambition</Tag>
-          <Tag color="purple">eslint</Tag>
-        </div>
+        <p>熟悉 vuejs，react，了解 nodejs.</p>
       </div>
 
       <div className="resume__view__contact">
         <p>
           邮箱：
-          <a href="mailto:zhuangtianyu1995@163.com">zhuangtianyu1995@163.com</a>
+          <a href="mailto:zhuangtianyu1995@163.com">
+            zhuangtianyu1995@163.com
+          </a>
         </p>
         <p>
           github：
