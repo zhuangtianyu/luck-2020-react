a1f01a80cd5119455198dca87c817bec139261ce
zhuangtianyu <zhuangtianyu1995@163.com>
2021-10-12 22:26:40 +0800

    feat: 首页添加备案号及工信部链接

--- src/views/intro/index.js
+++ src/views/intro/index.js
@@ -8,6 +8,13 @@ function IntroView () {
       <Link to="/home" className="home__link">
         每当浪潮来临的时候<br />你会不会也伤心→
       </Link>
+      <a
+        className="ICP"
+        target="_blank"
+        href="https://beian.miit.gov.cn/"
+      >
+        京ICP备18002046号-1
+      </a>
     </div>
   )
 }
